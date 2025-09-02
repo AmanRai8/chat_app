4c030085a58326eaff5b782c984d31910462835a
aman_sama <amanrai2002acr@gmail.com>
2025-09-02 10:10:16 +0545

    channels and DM enabled

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,23 +6,21 @@ import { clerkMiddleware } from "@clerk/express";
 import { functions, inngest } from "./config/inngest.js";
 import { serve } from "inngest/express";
 import chatRoutes from "./routes/chat.route.js";
-
-import cors from "cors";
-
 import * as Sentry from "@sentry/node";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-app.use(clerkMiddleware()); // req.auth will be available in the request object
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(clerkMiddleware());
 
 app.get("/debug-sentry", (req, res) => {
   throw new Error("My first Sentry error!");
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World! 123");
+  res.send("Hello World3");
 });
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
@@ -35,12 +33,12 @@ const startServer = async () => {
     await connectDB();
     if (ENV.NODE_ENV !== "production") {
       app.listen(ENV.PORT, () => {
-        console.log("Server started on port:", ENV.PORT);
+        console.log("server started on port: ", ENV.PORT);
       });
     }
   } catch (error) {
-    console.error("Error starting server:", error);
-    process.exit(1); // Exit the process with a failure code
+    console.error("Error starting server", error);
+    process.exit(1);
   }
 };
 
